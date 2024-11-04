1372f53c53b88d69c0ed1901e432b53c06c2c72c
xz719 <651300204@qq.com>
2024-11-04 17:01:16 +0800

    feat: 算法 9 10

--- 大前端知识文档/算法/【教程】前端算法与数据结构/draft.js
+++ 大前端知识文档/算法/【教程】前端算法与数据结构/draft.js
@@ -1,32 +1,23 @@
 /**
  * @param {ListNode} head
- * @return {ListNode}
+ * @return {boolean}
  */
-const deleteDuplicates = function (head) {
-  // 优先判断：0个或1个结点，直接返回，不可能存在重复
-  if (!head || !head.next) {
-    return head;
-  }
-  // 1. 设置 dummy 结点，dummy 是头结点的前驱
-  let dummy = new ListNode();
-  dummy.next = head;
-
-  // 2. 从 dummy 开始遍历链表，检查后续的两个结点
-  let cur = dummy;
-  // 检查后续两个结点是否存在重复，直到后续不存在两个结点
-  while (cur.next && cur.next.next) {
-    // 是否存在重复
-    if (cur.next.val === cur.next.next.val) {
-        // 若存在重复，则从第一个后继开始，一边删除重复结点一边继续判断
-        let val = cur.next.val;
-        while(cur.next && cur.next.val === val) {
-            cur.next = cur.next.next;
-        }
+// 入参是头结点 
+const hasCycle = function(head) {
+  // 设置遍历指针
+  let cur = head;
+  // 遍历
+  while(cur) {
+    if (!cur.once) {
+      // 没走过
+      cur.once = true;
+      cur = cur.next;
     } else {
-        // 否则，cur 向后移动
-        cur = cur.next;
+      // 走过，说明成环
+      return true;
     }
   }
 
-  return dummy.next;
-};
+  // 遍历完都没遇到走过的结点，说明不成环
+  return false;
+}
