e5776e6d08d90bcc52eb48fc321a3a1885497a09
xz719 <651300204@qq.com>
2024-11-20 13:54:11 +0800

    feat: 算法 21 22

--- 大前端知识文档/算法/【教程】前端算法与数据结构/draft.js
+++ 大前端知识文档/算法/【教程】前端算法与数据结构/draft.js
@@ -1,57 +1,20 @@
-// 工具函数：以基准值为轴心，划分左右子数组的过程
-function partition(arr, left, right) {
-  // 基准值默认取数组中间的元素
-  let pivotValue = arr[Math.floor(left + (left - right) / 2)];
+// 入参是物品的个数和背包的容量上限，以及物品的重量和价值数组
+function knapsack(n, c, w, value) {
+  // 保存计算结果的数组，这里数组长度为 c + 1 因为要考虑背包容量为0的情况
+  const dp = new Array(c + 1).fill(0);
 
-  // 左右指针
-  let i = left,
-    j = right;
+  let res = -Infinity; // 结果
 
-  // 在左右指针不越界的情况下
-  while (i <= j) {
-    // 左指针所指元素若小于基准值，则右移左指针
-    while (arr[i] < pivotValue) {
-      i++;
-    }
+  for (let i = 0; i < n; i++) {
+    for (let v = c; v >= w[i]; v--) {
+      dp[v] = Math.max(dp[v], dp[v - w[i]] + value[i]);
 
-    // 右指针所指元素若大于基准值，则左移右指针
-    while (arr[j] > pivotValue) {
-      j--;
-    }
-
-    // 若i<=j，则意味着基准值左边存在较大元素或右边存在较小元素，交换两个元素确保左右两侧有序
-    if (i <= j) {
-      swap(arr, i, j);
-      i++;
-      j--;
+      // 更新
+      if (res < dp[v]) {
+        res = dp[v];
+      }
     }
   }
 
-  // 返回左指针索引作为下一次划分左右子数组的依据
-  return i;
-}
-
-// 工具函数：交换数组中两个元素的位置
-function swap(arr, i, j) {
-  [arr[i], arr[j]] = [arr[j], arr[i]];
-}
-
-// 快速排序算法
-function quickSort(arr, left = 0, right = arr.length - 1) {
-  // 定义递归边界，若数组只有一个元素，则没有排序必要
-  if (arr.length > 1) {
-    // lineIndex表示下一次划分左右子数组的索引位
-    const lineIndex = partition(arr, left, right);
-    // 如果左边子数组的长度不小于1，则递归快排这个子数组
-    if (left < lineIndex - 1) {
-      // 左子数组以 lineIndex-1 为右边界
-      quickSort(arr, left, lineIndex - 1);
-    }
-    // 如果右边子数组的长度不小于1，则递归快排这个子数组
-    if (lineIndex < right) {
-      // 右子数组以 lineIndex 为左边界
-      quickSort(arr, lineIndex, right);
-    }
-  }
-  return arr;
+  return res;
 }
