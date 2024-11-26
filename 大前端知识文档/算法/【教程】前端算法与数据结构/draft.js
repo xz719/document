cc6f2a7d712fa4ddb956e1c2eb0644f959e3f72f
xz719 <651300204@qq.com>
2024-11-26 16:02:38 +0800

    feat: 算法 24

--- 大前端知识文档/算法/【教程】前端算法与数据结构/draft.js
+++ 大前端知识文档/算法/【教程】前端算法与数据结构/draft.js
@@ -1,20 +1,50 @@
-// 入参是物品的个数和背包的容量上限，以及物品的重量和价值数组
-function knapsack(n, c, w, value) {
-  // 保存计算结果的数组，这里数组长度为 c + 1 因为要考虑背包容量为0的情况
-  const dp = new Array(c + 1).fill(0);
-
-  let res = -Infinity; // 结果
-
-  for (let i = 0; i < n; i++) {
-    for (let v = c; v >= w[i]; v--) {
-      dp[v] = Math.max(dp[v], dp[v - w[i]] + value[i]);
-
-      // 更新
-      if (res < dp[v]) {
-        res = dp[v];
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+// 入参是二维数组
+const numIslands = function (grid) {
+  const moveX = [0, 1, 0, -1];
+  const moveY = [1, 0, -1, 0];
+  // 处理二维数组的边界情况
+  if (!grid || grid.length === 0 || grid[0].length === 0) {
+    return 0;
+  }
+  // 初始化岛屿数量
+  let count = 0;
+  // 缓存二维数组的行数和列数
+  let row = grid.length,
+    column = grid[0].length;
+  // 以行和列为线索，尝试“逐个”遍历二位数组中的坑位
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < column; j++) {
+      if (grid[i][j] === "1") {
+        // 每遇到1，就进入dfs，探索岛屿边界
+        dfs(grid, i, j);
+        // 每完成一个 dfs，就累加一个岛屿
+        count++;
       }
     }
   }
+  return count;
 
-  return res;
-}
+  // 编写探索岛屿边界的逻辑
+  function dfs(grid, i, j) {
+    // 如果试图探索的范围已经越界，则return
+    if (
+      i < 0 ||
+      i >= grid.length ||
+      j < 0 ||
+      j >= grid[0].length ||
+      grid[i][j] === "0"
+    ) {
+      return;
+    }
+    // 遍历过的坑位都置0，防止反复遍历
+    grid[i][j] = "0";
+    // 遍历完当前的1，继续去寻找下一个1
+    for (let k = 0; k < 4; k++) {
+      dfs(grid, i + moveX[k], j + moveY[k]);
+    }
+  }
+};
